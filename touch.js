09d0498df57ea364c0dc898ee853de335834be8a
Ben Carr <ben.carr2@ntlworld.com>
2013-11-01 13:05:34 +0000

    Update touch.js
    Might help if I could access some of the variables

--- touch.js
+++ touch.js
@@ -1,10 +1,10 @@
 document.onload = function(e){
-  var element = document.getElementById('el');
-  var hammertime = Hammer(el)
-  var transform = hammertime.on('transform', function(event){
+  element = document.getElementById('el');
+  hammertime = Hammer(el)
+  transform = hammertime.on('transform', function(event){
     alert('Hello!');
   }); 
-  var drag = hammertime.on('drag', function(event){
+  drag = hammertime.on('drag', function(event){
     console.log(event);
     alert('Hellish!');
   }); 

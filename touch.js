cac194b120baa5e84d8dfc7acc2bb814ee71122e
Ben Carr <ben.carr2@ntlworld.com>
2013-11-01 13:00:50 +0000

    Update touch.js
    Can't transform with one touch

--- touch.js
+++ touch.js
@@ -1,7 +1,11 @@
 document.onload = function(e){
   var element = document.getElementById('el');
-  var hammertime = Hammer(el).on('transform', function(event){
+  var hammertime = Hammer(el)
+  var transform = hammertime.on('transform', function(event){
     alert('Hello!');
+  }); 
+  var drag = hammertime.on('drag', function(event){
+    alert('Hellish!');
   }); 
 };
 

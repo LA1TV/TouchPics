d3edfbd3a241f1eb4a082839eb69c198b9f54297
Ben Carr <ben.carr2@ntlworld.com>
2013-11-03 23:22:51 +0000

    It would appear that the dragging is fully functional

--- touch.js
+++ touch.js
@@ -4,10 +4,15 @@ window.onload = function(e){
 	transform = hammertime.on('transform', function(event){
 		console.log(event);
 	}); 
+	tstart = document.body.addEventListener('touchstart', function(event){
+		console.log(event);
+		event.target.parentNode.offsetX = event.target.x - event.targetTouches[0].pageX;
+		event.target.parentNode.offsetY = event.target.y - event.targetTouches[0].pageY;
+	});
 	drag = hammertime.on('drag', function(event){
 		console.log(event);
-		event.target.parentNode.style.top = event.gesture.startEvent.center.pageY + event.gesture.deltaY;
-		event.target.parentNode.style.left = event.gesture.startEvent.center.pageX + event.gesture.deltaX;
+		event.target.parentNode.style.left = event.gesture.startEvent.center.pageX + event.gesture.deltaX + event.target.parentNode.offsetX;
+		event.target.parentNode.style.top = event.gesture.startEvent.center.pageY + event.gesture.deltaY + event.target.parentNode.offsetY;
 	}); 
 };
 

89407d5d3558ff202cb4df9fcb5578bf2e8bb043
Ben Carr <ben.carr2@ntlworld.com>
2013-11-03 19:05:20 +0000

    Got some kind of element movement going on will need to: Prevent scrolling Probably prevent mouse events

--- touch.js
+++ touch.js
@@ -1,11 +1,14 @@
 window.onload = function(e){
-  element = document.getElementById('el');
-  hammertime = Hammer(el, {prevent_mouseevents: true, transform_always_block: true})
-  transform = hammertime.on('transform', function(event){
-    console.log(event);
-  }); 
-  drag = hammertime.on('drag', function(event){
-    console.log(event);
-  }); 
+	element = document.getElementById('el');
+	hammertime = Hammer(document.body, {prevent_mouseevents: true, transform_always_block: true})
+	transform = hammertime.on('transform', function(event){
+		console.log(event);
+	}); 
+	drag = hammertime.on('drag', function(event){
+		console.log(event);
+		event.target.parentNode.posTop += event.gesture.deltaY
+		console.log(event.target.parentNode.posTop);
+		event.target.parentNode.style.top =event.target.parentNode.posTop;
+	}); 
 };
 

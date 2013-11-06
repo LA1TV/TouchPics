27a42ae9a17106d1b7b5c5ec6caaa6b06a9383a5
Ben Carr <ben.carr2@ntlworld.com>
2013-11-06 20:57:26 +0000

    Got some basic scaling working Got the fakemultitouch working

--- touch.js
+++ touch.js
@@ -1,12 +1,23 @@
 window.onload = function(e){
-	touchable = document.getElementById('test_touchable');
+	Hammer.plugins.fakeMultitouch();
+	Hammer.plugins.showTouches();
+
+	touchable = document.getElementById('test_touchable').firstElementChild;
+	touchable.scale = 1;
 	button = document.getElementById('test_button');
-	hammer_config = {prevent_mouseevents: true, transform_always_block: true, drag_block_horizontal: true, drag_block_vertical:true};
-	hammertime = Hammer(document.body, {prevent_mouseevents: true, transform_always_block: true, drag_block_horizontal: true, drag_block_vertical:true})
+	hammer_config = {prevent_mouseevents: false, transform_always_block: true, drag_block_horizontal: true, drag_block_vertical:true};
+
 	touchable.hammertime = Hammer(touchable, hammer_config);
 	button.hammertime = Hammer(button, hammer_config);
 	
+	transform_start = touchable.hammertime.on('transformstart', function(event){
+		event.target.startScale = event.target.scale;
+		console.log(event);
+	});
 	transform = touchable.hammertime.on('transform', function(event){
+		event.target.scale = event.target.startScale * event.gesture.scale;
+		event.target.width = event.target.naturalWidth * event.target.scale;
+		event.target.height = event.target.naturalHeight * event.target.scale;
 		console.log(event);
 	}); 
 	drag_start = touchable.hammertime.on('dragstart', function(event){

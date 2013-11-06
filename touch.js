53fe52dc2c5cde01269f844a09a84c1963f413c9
Ben Carr <ben.carr2@ntlworld.com>
2013-11-06 22:48:35 +0000

    Added some TODOs Removed the second button, will add it back when its actually needed

--- touch.js
+++ touch.js
@@ -2,6 +2,9 @@ window.onload = function(e){
 	Hammer.plugins.fakeMultitouch();
 	Hammer.plugins.showTouches();
 
+	//TODO Create the divs in JS
+	//TODO Ability to close divs
+	//TODO Image menu
 	touchable = document.getElementById('test_touchable').firstElementChild;
 	touchable.scale = 1;
 	touchable.parentNode.pos = {};
@@ -12,12 +15,16 @@ window.onload = function(e){
 	button.hammertime = Hammer(button, hammer_config);
 	
 	transform_start = touchable.hammertime.on('transformstart', function(event){
+		//TODO Tidy up the spaghetti
 		event.target.startScale = event.target.scale;
 		event.target.parentNode.pos.startLeft = event.target.parentNode.pos.left;
 		event.target.parentNode.pos.startTop = event.target.parentNode.pos.top;
-		console.log(event);
+		//console.log(event);
 	});
 	transform = touchable.hammertime.on('transform', function(event){
+		//TODO Add a minimum size for when scaling
+		//TODO Scaling about gesture center
+		//TODO Rotation
 		event.target.scale = event.target.startScale * event.gesture.scale;
 		event.target.width = event.target.naturalWidth * event.target.scale;
 		event.target.height = event.target.naturalHeight * event.target.scale;
@@ -25,21 +32,22 @@ window.onload = function(e){
 		event.target.parentNode.style.left = event.target.parentNode.pos.left;
 		event.target.parentNode.pos.top = event.target.parentNode.pos.startTop - event.target.scale * event.target.naturalHeight/2 + event.target.startScale  * event.target.naturalHeight/2;
 		event.target.parentNode.style.top = event.target.parentNode.pos.top;
-		console.log(event);
+		//console.log(event);
 	}); 
 	drag_start = touchable.hammertime.on('dragstart', function(event){
-		console.log(event);
+		//console.log(event);
 		event.target.parentNode.pos.offsetX = event.target.x - event.gesture.center.pageX;
 		event.target.parentNode.pos.offsetY = event.target.y - event.gesture.center.pageY;
 	});
 	drag = touchable.hammertime.on('drag', function(event){
-		console.log(event);
+		//console.log(event);
 		event.target.parentNode.pos.left = event.gesture.startEvent.center.pageX + event.gesture.deltaX + event.target.parentNode.pos.offsetX;
 		event.target.parentNode.pos.top = event.gesture.startEvent.center.pageY + event.gesture.deltaY + event.target.parentNode.pos.offsetY;
 		event.target.parentNode.style.left = event.target.parentNode.pos.left;
 		event.target.parentNode.style.top = event.target.parentNode.pos.top;
 	}); 
 	close = button.hammertime.on('tap', function(event){
+		//TODO Close divs
 		alert("This doesn't do much yet");
 	});
 };

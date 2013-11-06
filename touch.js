f0fc8ca385670ab63505ab41d2c1ee398d0b5ef9
Ben Carr <ben.carr2@ntlworld.com>
2013-11-06 21:56:47 +0000

    Items are now scaled about their centre Buts its quite a mess

--- touch.js
+++ touch.js
@@ -4,6 +4,7 @@ window.onload = function(e){
 
 	touchable = document.getElementById('test_touchable').firstElementChild;
 	touchable.scale = 1;
+	touchable.parentNode.pos = {};
 	button = document.getElementById('test_button');
 	hammer_config = {prevent_mouseevents: false, transform_always_block: true, drag_block_horizontal: true, drag_block_vertical:true};
 
@@ -12,23 +13,31 @@ window.onload = function(e){
 	
 	transform_start = touchable.hammertime.on('transformstart', function(event){
 		event.target.startScale = event.target.scale;
+		event.target.parentNode.pos.startLeft = event.target.parentNode.pos.left;
+		event.target.parentNode.pos.startTop = event.target.parentNode.pos.top;
 		console.log(event);
 	});
 	transform = touchable.hammertime.on('transform', function(event){
 		event.target.scale = event.target.startScale * event.gesture.scale;
 		event.target.width = event.target.naturalWidth * event.target.scale;
 		event.target.height = event.target.naturalHeight * event.target.scale;
+		event.target.parentNode.pos.left = event.target.parentNode.pos.startLeft - event.target.scale * event.target.naturalWidth/2 + event.target.startScale  * event.target.naturalWidth/2;
+		event.target.parentNode.style.left = event.target.parentNode.pos.left;
+		event.target.parentNode.pos.top = event.target.parentNode.pos.startTop - event.target.scale * event.target.naturalHeight/2 + event.target.startScale  * event.target.naturalHeight/2;
+		event.target.parentNode.style.top = event.target.parentNode.pos.top;
 		console.log(event);
 	}); 
 	drag_start = touchable.hammertime.on('dragstart', function(event){
 		console.log(event);
-		event.target.parentNode.offsetX = event.target.x - event.gesture.center.pageX;
-		event.target.parentNode.offsetY = event.target.y - event.gesture.center.pageY;
+		event.target.parentNode.pos.offsetX = event.target.x - event.gesture.center.pageX;
+		event.target.parentNode.pos.offsetY = event.target.y - event.gesture.center.pageY;
 	});
 	drag = touchable.hammertime.on('drag', function(event){
 		console.log(event);
-		event.target.parentNode.style.left = event.gesture.startEvent.center.pageX + event.gesture.deltaX + event.target.parentNode.offsetX;
-		event.target.parentNode.style.top = event.gesture.startEvent.center.pageY + event.gesture.deltaY + event.target.parentNode.offsetY;
+		event.target.parentNode.pos.left = event.gesture.startEvent.center.pageX + event.gesture.deltaX + event.target.parentNode.pos.offsetX;
+		event.target.parentNode.pos.top = event.gesture.startEvent.center.pageY + event.gesture.deltaY + event.target.parentNode.pos.offsetY;
+		event.target.parentNode.style.left = event.target.parentNode.pos.left;
+		event.target.parentNode.style.top = event.target.parentNode.pos.top;
 	}); 
 	close = button.hammertime.on('tap', function(event){
 		alert("This doesn't do much yet");

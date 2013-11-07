d9b8036165a20ead8e6834bb20cf1279de14ea89
Ben Carr <ben.carr2@ntlworld.com>
2013-11-07 21:04:01 +0000

    Got images moving again

--- src/images.js
+++ src/images.js
@@ -5,11 +5,12 @@ images = function(e){
     Hammer.plugins.fakeMultitouch();
     
     touchable = document.getElementById('test_item');
-    img = new TouchImage(touchable, "", 0, 0, 1);
+    img = new TouchImage(touchable, touchable.firstElementChild, "", 0, 0, 1);
 };
 
-TouchImage = function(el, src, x, y, scale){
+TouchImage = function(el, img, src, x, y, scale){
     this.el = el;
+    this.img = img;
     this.src = src;
     this.transform = {a:1, b:0, c:0, d:1, e:0, f:0};
     this.transform_matrix = "matrix("+this.transform.a+","+this.transform.b+","+this.transform.c+","+this.transform.d+","+this.transform.e+","+this.transform.f+")";
@@ -23,20 +24,18 @@ TouchImage = function(el, src, x, y, scale){
         ang: 0,
         startAng: 0
     };  
-    this.hammertime = Hammer(el.firstElementChild, hammer_config);
+    this.hammertime = Hammer(this.img, hammer_config);
     this.hammer = {};
     this.hammer.dragstart = this.hammertime.on('dragstart', this.dragStart());
     this.hammer.drag = this.hammertime.on('drag', this.drag());
 };
 
 TouchImage.prototype.updateTransform = function(){
-    var that = this;
-    return function(){
-        that.transform.e = that.pos.x;
-        that.transform.f = that.pos.y;
-        that.transform_matrix = "matrix("+this.transform.a+","+this.transform.b+","+this.transform.c+","+this.transform.d+","+this.transform.e+","+this.transform.f+")";
-        that.el.style.webkitTransform = that.transform_matrix;
-    }
+    this.transform.e = this.pos.x;
+    this.transform.f = this.pos.y;
+    this.transform_matrix = "matrix("+this.transform.a+","+this.transform.b+","+this.transform.c+","+this.transform.d+","+this.transform.e+","+this.transform.f+")";
+    this.el.style.webkitTransform = this.transform_matrix;
+    console.log(this.matrix_transform);
 };
 
 TouchImage.prototype.dragStart = function(){

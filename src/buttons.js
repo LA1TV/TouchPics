956f3328d933072e618c8eeb7410a0324c8f0adb
Ben Carr <ben.carr2@ntlworld.com>
2013-11-08 23:24:43 +0000

    Added the base constructor code for the AnimTouchButton Trying to add css specific to the AnimTouchButton second image

--- src/buttons.js
+++ src/buttons.js
@@ -28,7 +28,30 @@ TouchButton.prototype.callFunc = function(){
 
 AnimTouchButton = function(src1, src2, right, top, func){
     //TODO Allow animation to be defined
+    this.src1 = src1;
+    this.src2 = src2;
+    this.right = right;
+    this.top = top;
+    this.func = func;
     
+    this.div = document.createElement('div');
+    this.div.className = "button";
+    this.div.style.top = this.top;
+    this.div.style.right = this.right;
+    
+    this.img1 = document.createElement('img');
+    this.img1.className = "button";
+    this.img1.src = this.src1;
+    
+    this.img2 = document.createElement('img');
+    this.img2.className = "button over";
+    this.img2.src = this.src2;
+    
+    this.div.appendChild(this.img1);
+    this.div.appendChild(this.img2);
+    
+    this.hammer = Hammer(this.div, hammer_config);
+    this.hammer.on('tap', this.callFunc());
 }
 
 AnimTouchButton.prototype = new TouchButton();

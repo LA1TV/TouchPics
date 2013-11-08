e854f0411ac5a392583c63ce5b20fcb6087f5da7
Ben Carr <ben.carr2@ntlworld.com>
2013-11-08 23:07:58 +0000

    Added buttons.js to index.html Fleshed out TouchButton with div creation stuff and callback calling stuff

--- src/buttons.js
+++ src/buttons.js
@@ -1,6 +1,27 @@
-TouchButton = function(img, right, top, func){
-    this.img = img;
+TouchButton = function(src, right, top, func){
+    this.src = src;
     this.right = right;
     this.top = top;
     this.func = func;
+    
+    this.div = document.createElement('div');
+    this.div.className = "button";
+    this.div.style.top = this.top;
+    this.div.style.right = this.right;
+    
+    this.img = document.createElement('img');
+    this.img.className = "button";
+    this.img.src = this.src;
+    
+    this.div.appendChild(this.img);
+    
+    this.hammer = Hammer(this.div, hammer_config);
+    this.hammer.on('tap', this.callFunc);
+};
+
+TouchButton.prototype.callFunc = function(){
+    var that = this;
+    return function(event){
+        that.func(event);   
+    };
 };

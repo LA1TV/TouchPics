3c29b52424b8732eb5de307092243f3dd4001572
Ben Carr <ben.carr2@ntlworld.com>
2013-11-15 01:22:57 +0000

    Made the ImageMenuButton create some elements are call some currently non-existant functions

--- src/menu_buttons.js
+++ src/menu_buttons.js
@@ -1,9 +1,17 @@
 ImageMenuButton = function(src, dim, menu, manager){
     this.src = src;
     this.dim = dim;
+    this.menu = menu;
     this.manager = manager;
     
-    this.div = document.createElement
+    this.div = document.createElement('div');
+    this.div.className = 'menu button';
+    this.img = document.createElemnt('img');
+    this.img.src = this.src;
+    this.img.className = 'menu button';
+    this.div.appendChild(this.img);
+    
+    this.img.onload = this.menu.addButtonDiv(this);
 };
 
 ImageMenuButton.prototype.onTap = function(){
@@ -11,5 +19,12 @@ ImageMenuButton.prototype.onTap = function(){
     return function(event){
         //TODO x, y, scale not hard coded
         that.manager.newImage(that.src, 50, 50, 1.0);
+    };
+};
+
+ImageMenuButton.prototype.loaded = function(){
+    var that = this;
+    return function(e){
+        that.div.appendChild(that.img);  
     };
 };

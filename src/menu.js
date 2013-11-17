62d1b3131737dc81508cbb0f93102ed15360bfb1
Ben Carr <ben.carr2@ntlworld.com>
2013-11-17 01:21:28 +0000

    Made the image menu center itself horizontally using a container Fixed a small error with adding the TouchImageButton to the menu

--- src/menu.js
+++ src/menu.js
@@ -1,17 +1,29 @@
-Menu = function(width, height, bottom){
+Menu = function(width, height, bottom, manager){
     this.width = width;
     this.height = height;
     this.bottom = bottom;
+    this.manager = manager;
     this.buttons = {};
     
     this.div = document.createElement('div');
     this.div.className = 'imagemenu';
     this.div.style.width = width;
     this.div.style.height = height;
-    this.div.style.bottom = bottom;
-    document.body.appendChild(this.div);
+    
+    this.container = document.createElement('div');
+    this.container.className = 'imagemenucontainer';
+    this.container.style.width = '100%';
+    this.container.style.bottom = this.bottom;
+    this.container.appendChild(this.div);
+    
+    document.body.appendChild(this.container);
 };
 
 Menu.prototype.createButton = function(src){
-    
+    var button = new ImageMenuButton(src, {width:100, height:50}, this, this.manager)
+    this.buttons[src] = button;
+};
+
+Menu.prototype.addButtonDiv = function(div){
+    this.div.appendChild(div);
 };

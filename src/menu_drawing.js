e8696e2d0927bd6984fb86d20fc3ea0a7b98f1a1
Ben Carr <ben.carr2@ntlworld.com>
2014-01-24 17:46:45 +0000

    Add a nonfunctional drawing button Reworked the drawing menu to have some padding

--- src/menu_drawing.js
+++ src/menu_drawing.js
@@ -10,8 +10,8 @@ DrawingMenu = function(width, height, manager){
     
     this.drawing_menu_swipe_area = document.createElement('div');
     this.drawing_menu_swipe_area.className = "drawing_menu_swipe_area";
-    this.drawing_menu_swipe_area.style['height'] = height + 50;
-    this.drawing_menu_swipe_area.style['width'] = width + 75;
+    this.drawing_menu_swipe_area.style['height'] = height + 50 + 20;
+    this.drawing_menu_swipe_area.style['width'] = width + 75 + 20;
     this.drawing_menu_swipe_area.appendChild(this.div);
     
     document.body.appendChild(this.drawing_menu_swipe_area);
@@ -21,13 +21,15 @@ DrawingMenu = function(width, height, manager){
     this.hammer = {};
     this.hammer.close = this.swipe_hammertime.on('swipeleft', this.hideMenu());
     this.hammer.open = this.swipe_hammertime.on('swiperight', this.showMenu());
+    
+    this.addButton("assets/draw.svg", 0, 0, console.log);
 };
 
 DrawingMenu.prototype.hideMenu = function(){
     var that = this;
     return function(){
         that.open = false;
-        that.div.style['left'] = -that.width; 
+        that.div.style['left'] = -that.width - 20; 
     };
 };
 
@@ -38,3 +40,8 @@ DrawingMenu.prototype.showMenu = function(){
         that.div.style['left'] = 0;  
     };
 };
+
+DrawingMenu.prototype.addButton = function(src, top, right, func){
+    button = new TouchButton(src, top, right, func);
+    this.div.appendChild(button.div);
+}
